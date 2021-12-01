6642bbc97ccc5f5ec9efdcc1c6b40ae4b97c812f
Kidus Worku <kidusmik@gmail.com>
2021-12-01 16:01:13 +0000

    Fix style

--- utils_num.c
+++ utils_num.c
@@ -9,6 +9,7 @@
 int powrd(int n)
 {
 	int i, pwrd;
+
 	i = 0;
 	pwrd = 1;
 
@@ -21,27 +22,33 @@ int powrd(int n)
 	return (pwrd);
 }
 
+/**
+* iton - converts a char to int
+* @c: character
+*
+* Return: coverted int, otherwise -1
+*/
 int iton(char c)
 {
 	if (c == '0')
 		return (0);
 	else if (c == '1')
 		return (1);
-        else if (c == '2')
-                return (2);
-        else if (c == '3')
-                return (3);
-        else if (c == '4')
-                return (4);
-        else if (c == '5')
-                return (5);
-        else if (c == '6')
-                return (6);
-        else if (c == '7')
-                return (7);
-        else if (c == '8')
-                return (8);
-        else if (c == '9')
-                return (9);
+	else if (c == '2')
+		return (2);
+	else if (c == '3')
+		return (3);
+	else if (c == '4')
+		return (4);
+	else if (c == '5')
+		return (5);
+	else if (c == '6')
+		return (6);
+	else if (c == '7')
+		return (7);
+	else if (c == '8')
+		return (8);
+	else if (c == '9')
+		return (9);
 	return (-1);
 }
--- utils_sys.c
+++ utils_sys.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+* printerr - prints error and exits
+* @str: error message
+*
+* Return: Always Void
+*/
 void printerr(char *str)
 {
 	printf("%s\n", str);
